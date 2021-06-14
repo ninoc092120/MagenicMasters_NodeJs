353fb67fde492fe76f909961d584a13eed35bd73
NinoC <ninoc@magenic.com>
2021-06-14 16:07:43 +0800

    remove commented lines

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ const yargs = require("yargs");
 const LoanCalculator = require("./loan-calculator");
 const argv = yargs.argv;
 
-// console.log(argv);
-
-// console.log(argv['bankName']);
-// console.log(argv['loanAmount']);
-// console.log(argv['loanTerm']);
 const bankName = argv['bankName'];
 const loanAmount = argv['loanAmount'];
 const loanTerm = argv['loanTerm'];
